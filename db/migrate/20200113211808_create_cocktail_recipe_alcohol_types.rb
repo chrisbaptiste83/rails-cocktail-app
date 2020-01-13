463f0482566457f477fae6703ec0d53bb8917e52
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-13 14:47:22 -0700

    modify migration files and run migrations

--- db/migrate/20200113211808_create_cocktail_recipe_alcohol_types.rb
+++ db/migrate/20200113211808_create_cocktail_recipe_alcohol_types.rb
@@ -1,7 +1,8 @@
 class CreateCocktailRecipeAlcoholTypes < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktail_recipe_alcohol_types do |t|
-
+      t.integer :cocktail_recipe_id 
+      t.integer :alcohol_type_id 
       t.timestamps
     end
   end
