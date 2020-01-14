dc1cf8ad67c332965001f14c9c5a69f0691e60c1
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-14 12:04:24 -0700

    add new, create, chow, and index actions to cocktail recipe controller. Also add strong params

--- app/controllers/cocktail_recipes_controller.rb
+++ app/controllers/cocktail_recipes_controller.rb
@@ -1,2 +1,27 @@
-class CocktailRecipesController < ApplicationController
+class CocktailRecipesController < ApplicationController 
+   
+    def new 
+        @cocktail_recipe = CocktailRecipe.new 
+    end 
+
+    def create
+        @cocktail_recipe = CocktailRecipe.new(cocktail_recipe_params) 
+        @coktail_recipe.save 
+        redirect_to cocktail_recipe_path(@cocktail_recipe)
+      end
+    
+      def show
+        @cocktail_recipe = CocktailRecipe.find(params[:id])
+      end 
+
+    def index 
+        @cocktail_recipes = CocktailRecipe.all  
+
+      private
+
+      def cocktail_recipe_params
+        params.require(:cocktail_recipe).permit(:title, :directions, :description)
+      end
+
+end
 end
