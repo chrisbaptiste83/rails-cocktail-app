94b8701fb5fddbbe3ae597f5cc61030ba9f928b8
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-14 11:26:54 -0700

    Add new, create, and show actions to user controller. Create user new and show views.

--- app/models/cocktail_recipe.rb
+++ app/models/cocktail_recipe.rb
@@ -3,5 +3,5 @@ class CocktailRecipe < ApplicationRecord
     belongs_to :category 
     has_many :comments 
     has_many :cocktail_recipe_alcohol_types 
-    has_many :alcohol_types :through => :cocktail_recipe_alcohol_types
+    has_many :alcohol_types, :through => :cocktail_recipe_alcohol_types
 end
