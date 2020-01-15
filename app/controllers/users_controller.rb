cef895cd3a8d54cc96425ac619137a233d117cb7
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-14 17:22:25 -0700

    add validations to user model and validation errors on sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,14 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        @user.save 
+        
+    if @user.valid?
+        @user.save
         redirect_to user_path(@user)
+      else
+        render :new
       end
+    end
     
       def show
         @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord 
     has_many :cocktail_recipes 
     has_many :comments 
+    validates :username, presence: true    
+    validates :email, presence: true
+    validates :password, length: { in: 8..15 }
 end
