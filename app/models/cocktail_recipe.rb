a9d1d7cc5758a920a37dddefb2aada4ebb9a7a7e
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-15 09:18:33 -0700

    add accepts nested attributes macro to cocktail recipe model

--- app/models/cocktail_recipe.rb
+++ app/models/cocktail_recipe.rb
@@ -4,7 +4,18 @@ class CocktailRecipe < ApplicationRecord
     has_many :comments 
     has_many :cocktail_recipe_alcohol_types 
     has_many :alcohol_types, :through => :cocktail_recipe_alcohol_types 
+    has_many :ingredients
 
-    #validates :title, :description, :directions, presence: true
+    accepts_nested_attributes_for :ingredients
+
+    #validates :title, :description, :directions, presence: true  
+
+    def category_name=(name)
+        self.category = Category.find_or_create_by(name: name)
+      end
+    
+      def category_name
+         self.category ? self.category.name : nil
+      end
 
 end
