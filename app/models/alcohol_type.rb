171e4994f32da02226efb0d86bdc6fc9940d04db
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-15 19:19:05 -0700

    add initialize action in alcohol type controller

--- app/models/alcohol_type.rb
+++ app/models/alcohol_type.rb
@@ -2,4 +2,9 @@ class AlcoholType < ApplicationRecord
     has_many :cocktail_recipe_alcohol_types 
     has_many :cocktail_recipes, :through => :cocktail_recipe_alcohol_types 
 
+    def new 
+        @alcohol_type = AlcoholType.new 
+    end 
+    
+
 end
