7067366093c95d7466612cff2420b92af177b0ff
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-16 18:06:20 -0700

    add links to recipe show page and category in recipe index page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :cocktail_recipes
   resources :categories
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
+  root to: 'users#new'
 end
