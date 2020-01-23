f436ea6a7766ab85f99b036f96426557e1036d61
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-22 19:35:57 -0700

    configure new routes and incorporate partial form partial into edit vies

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :alcohol_types
-  resources :comments
-  resources :ingredients
-  resources :cocktail_recipes
-  resources :categories
-  resources :users
+  devise_for :users 
+
+  resources :alcohol_types, only: [:show, :index] 
+
+  resources :cocktail_recipe_alcohol_types, only: [:show, :index]
+
+  resources :cocktail_recipes do
+    resources :comments 
+  end 
+
+  resources :users, only: [:show] do
+    resources :cocktail_recipes
+  end 
+
+  resources :categories, only: [:show, :index]
+  resources :users, only: [:show, :index]
   root to: 'users#home'
 end
