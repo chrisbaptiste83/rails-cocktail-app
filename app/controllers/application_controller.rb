1c015cd60e4bbc1ebb0ff40e9235233b06145df0
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-01-23 16:17:36 -0700

    update category show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller? 
 
-    def after_sign_in_path_for(resource)
-        cocktail_recipes_path
-      end
+  
+
+
     
     protected
   
