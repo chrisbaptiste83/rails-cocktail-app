d7695d66dc95b7ea03d87176f8cbcc45891dc5ef
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-02-02 10:24:14 -0700

    update ingredients controller for search functionality

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,11 @@
 class IngredientsController < ApplicationController 
 
     def index 
-        @ingredients = Ingredient.all 
-    end 
+        if params[:search]
+            @ingredients = Ingredient.search(params[:search]).order("created_at DESC")
+        else @ingredients = Ingredient.all.order(:name) 
+        end 
+    end
 
     def show
         @ingredient = Ingredient.find(params[:id]) 
