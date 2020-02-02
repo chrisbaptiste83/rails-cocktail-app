eae9f17335662ae574e3d57dcd32d910ba379c45
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-02-02 10:10:33 -0700

    add counter cache macro to comment model for comment count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord 
-    belongs_to :cocktail_recipe 
-    belongs_to :user 
+    belongs_to :cocktail_recipe, :counter_cache => true  
+    belongs_to :user
 
     accepts_nested_attributes_for :user, reject_if: :all_blank
 
