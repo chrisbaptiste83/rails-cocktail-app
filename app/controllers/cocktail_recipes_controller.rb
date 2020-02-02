52959dff7f4c6826f4236a7383bd053ba71c4a24
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-02-02 10:23:20 -0700

    update controller for search functionality and modify strong params to include attachment avater

--- app/controllers/cocktail_recipes_controller.rb
+++ app/controllers/cocktail_recipes_controller.rb
@@ -11,13 +11,12 @@ class CocktailRecipesController < ApplicationController
 
 
     def create
-        @cocktail_recipe = current_user.cocktail_recipes.new(cocktail_recipe_params)
+        @cocktail_recipe = current_user.cocktail_recipes.new(cocktail_recipe_params) 
         if @cocktail_recipe.save
-          @cocktail_recipe.add_ingredients_to_recipe(recipe_ingredient_params) 
+          @cocktail_recipe.add_ingredients_to_recipe(recipe_ingredient_params)
+          redirect_to @cocktail_recipe, notice: "Your recipe has successfully been added" 
+        else 
           
-        
-          redirect_to @cocktail_recipe, notice: "Your recipe has successfully been added"
-        else
           @cocktail_recipe = CocktailRecipe.new
           redirect_to new_cocktail_recipe_url
         end
@@ -29,15 +28,15 @@ class CocktailRecipesController < ApplicationController
       end 
 
     def index 
-
       @users = User.all 
-
-        if !params[:user].blank?
-          @cocktail_recipes = CocktailRecipe.by_user(params[:user]) 
+        if params[:search]
+          @cocktail_recipes = CocktailRecipe.search(params[:search]).order("created_at DESC")
+        elsif !params[:user].blank?
+          @cocktail_recipes = CocktailRecipe.by_user(params[:user]).order(:title)
         elsif params[:user_id]
-            @cocktail_recipes = User.find(params[:user_id]).cocktail_recipes
+            @cocktail_recipes = User.find(params[:user_id]).cocktail_recipes.order('title ASC')
           else
-            @cocktail_recipes = CocktailRecipe.all
+            @cocktail_recipes = CocktailRecipe.all.order(:title)
           end
         end
 
@@ -65,12 +64,12 @@ class CocktailRecipesController < ApplicationController
       private
 
       def cocktail_recipe_params
-        params.require(:cocktail_recipe).permit(:user_id, :category_name, :title, :directions, :description) 
+        params.require(:cocktail_recipe).permit(:user_id, :category_name, :title, :directions, :description, :avatar) 
       end 
 
       def recipe_ingredient_params
-            params.require(:cocktail_recipe).permit(recipe_ingredients_attributes: [:quantity, :ingredient_id, ingredient: [:name]])
-          end
-        
+        params.require(:cocktail_recipe).permit(recipe_ingredients_attributes: [:quantity, :ingredient_id, ingredient: [:name]])
+      end
+     
 
 end
