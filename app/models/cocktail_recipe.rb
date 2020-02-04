2d73bfc1de380a711de1ccb946083073d825296b
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-02-04 07:39:23 -0700

    modify next and previous methods to use title instead of recipe id, add ingredients validation

--- app/models/cocktail_recipe.rb
+++ app/models/cocktail_recipe.rb
@@ -8,7 +8,7 @@ class CocktailRecipe < ApplicationRecord
     accepts_nested_attributes_for :recipe_ingredients, reject_if: lambda {|attributes| attributes['name'].blank?}
     accepts_nested_attributes_for :ingredients, reject_if: lambda {|attributes| attributes['name'].blank?}
 
-    validates :title, :description, :directions, presence: true  
+    validates :title, :description, :directions, :ingredients, presence: true  
 
     has_attached_file :avatar, :styles => { :medium => "200x200#"}
 
@@ -16,63 +16,62 @@ class CocktailRecipe < ApplicationRecord
 
     def category_name=(name)
         self.category = Category.find_or_create_by(name: name)
-      end
+    end
     
-      def category_name
+    def category_name
          self.category ? self.category.name : nil
-      end 
+    end 
 
-      def self.five_latest_cocktail_recipes
-        order('created_at desc')[0...5]
-      end 
+    def self.five_latest_cocktail_recipes
+       order('created_at desc')[0...5]
+    end 
 
-      def self.most_comments
-        order("comments_count DESC").first
-      end 
+    def self.most_comments
+       order("comments_count DESC").first
+    end 
 
-      def previous
-        CocktailRecipe.where("id < ?", id).last
-      end
+    def previous
+        CocktailRecipe.all.order(:title).where("title < ?", title).last
+    end
       
 
-      def next
-        CocktailRecipe.all.where("id > ?", id).first
-      end
+    def next
+        CocktailRecipe.all.order(:title).where("title > ?", title).first
+    end
 
 
-      def self.by_user(user_id)
-        where(user: user_id)
-      end
+    def self.by_user(user_id)
+      where(user: user_id)
+    end
 
-      def self.of_the_day
-        order('RANDOM()').first 
-      end
+    def self.of_the_day
+      order('RANDOM()').first 
+    end
 
      
-      def self.search(search)
-        where("name LIKE ?", "%#{search}%") 
-      end
+    def self.search(search)
+      where("title LIKE ?", "%#{search}%") 
+    end
 
-
-      def delete_ingredients_from_recipe
+    def delete_ingredients_from_recipe
         ingredients.size.times do
         ingredient = RecipeIngredient.find_by(cocktail_recipe_id: self.id)
         ingredient.delete
-      end
+     end
     end 
 
     def add_ingredients_to_recipe(ingredients_params)
-      delete_ingredients_from_recipe
+      delete_ingredients_from_recipe 
+
       ingredients_params[:recipe_ingredients_attributes].each do |k, recipe_ingredient|
         if recipe_ingredient[:ingredient][:name].present?
           ingredient_name = recipe_ingredient[:ingredient][:name]
           ingredient = Ingredient.find_or_create_by(name: ingredient_name)
         elsif recipe_ingredient[:ingredient_id].present?
-          ingredient = Ingredient.find_by(id: recipe_ingredient[:ingredient_id])
+          ingredient = Ingredient.find_by(id: recipe_ingredient[:ingredient_id]) 
         end
-  
         if recipe_ingredient[:quantity].present?
-          RecipeIngredient.create(quantity: recipe_ingredient[:quantity], ingredient_id: ingredient.id, cocktail_recipe_id: self.id )
+          RecipeIngredient.create(quantity: recipe_ingredient[:quantity], ingredient_id: ingredient.id, cocktail_recipe_id: self.id ) 
         end
   
       end
