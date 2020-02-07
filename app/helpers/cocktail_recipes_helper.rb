348e7f8b45a6e7b9803c445c0d6827ca27ce9008
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-02-06 20:30:56 -0700

    add created at helper method in cocktail recipes helpers

--- app/helpers/cocktail_recipes_helper.rb
+++ app/helpers/cocktail_recipes_helper.rb
@@ -2,6 +2,10 @@ module CocktailRecipesHelper
 
     def last_updated(cocktail_recipe)
         cocktail_recipe.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+      end  
+
+      def created_at(cocktail_recipe)
+        cocktail_recipe.created_at.strftime("%A, %b %e, at %l:%M %p")
       end 
 
 end
