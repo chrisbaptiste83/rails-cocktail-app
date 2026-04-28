f0a674c931bddee53b18115ce8df81dd97f29eef
chrisbaptiste83 <chrisbaptiste83@gmail.com>
2026-04-28 11:37:39 -0700

    Use local storage for Cocktail production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on Cloudinary (see config/storage.yml for options).
-  config.active_storage.service = :cloudinary
+  # Store uploaded files on the persistent Kamal volume.
+  config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
